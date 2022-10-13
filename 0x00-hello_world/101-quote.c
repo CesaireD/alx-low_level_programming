2fdf71efb9fa41896a27176015b83a0da0bf6313
CesaireD <85783128+CesaireD@users.noreply.github.com>
2022-10-13 11:18:45 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,22 +1,11 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <unistd.h>
 /**
- * main - main 
- *
- * Return: 1
-**/
+ * main - Entry point
+ * Return:1
+ */
 int main(void)
 {
-	char *string;
-	char str1[] = "and that piece of art is useful\"";
-	char str2[] = "- Dora Korpar, 2015-10-2019";
-
-	string = malloc(strlen(str1) + strlen(str2) + 1);
-	strcpy(string, str1);
-	strcpy(string, str2);
-	write(2, string, sizeof(string));
-	
-	return (1);
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
 }
