b95994171304aead9342e8e3f0da772e1c8468b3
CesaireD <cesairedenon08@gmail.com>
2022-10-18 23:51:57 +0100

    16

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+/**
+ * main - fibonacci <3
+ *
+ * Purpose - no hardcode
+ *
+ * Return:  (Success)
+ */
+
+int main(void)
+{
+	unsigned long int i, b1, b2, a1, a2;
+	unsigned long int b = 1;
+	unsigned long int aft = 2;
+	unsigned long int l = 1000000000;
+
+	printf("%lu", b);
+
+	for (i = 1; i < 91; i++)
+	{
+		printf(", %lu", a);
+		a += b;
+		b = a - b;
+	}
+
+	b1 = (b / l);
+	b2 = (b % l);
+	a1 = (a / l);
+	a2 = (a % l);
+
+	for (i = 92; i < 99; ++i)
+	{
+		printf(", %lu", a1 + (a2 / l));
+		printf("%lu", a2 % l);
+		aft1 = a1 + b1;
+		bef1 = a1 - b1;
+		aft2 = a2 + b2;
+		bef2 = a2 - b2;
+	}
+	printf("\n");
+	return (0);
+}
